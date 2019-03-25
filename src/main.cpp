5149a923453ec69d2a9f2162cd2ec3b246f4570c
Adrian Joiner <adrianjoiner@gmail.com>
2019-03-25 21:59:02 +0100

    [wip]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include "MPU9250.h"
+#include <Adafruit_BMP280.h> //include the Adafruit BMP280 library
+#include "Adafruit_PCD8544.h"
+#include "Adafruit_GFX.h"
 
 void setup() {
   // put your setup code here, to run once:
